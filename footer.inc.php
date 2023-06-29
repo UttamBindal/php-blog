5ab23324120ffe721cfdcaa0733fe7aab8a18b6c
UttamBindal <ping.uttambindal@gmail.com>
2023-06-29 16:36:36 +0530

    Changes made

---
+++ footer.inc.php
@@ -0,0 +1,5 @@
+<footer class="footer text-center">
+  <div class="container">
+    <p>This project is made in partial fulfillment for the requirements for the sixth semester.</p>
+  </div>
+</footer>
---
+++ user/includes/footer.inc.php
@@ -0,0 +1,14 @@
+<!-- jQuery -->
+<script src="../assets/jquery/jquery.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="../assets/bootstrap/js/bootstrap.js"></script>
+<!-- JS -->
+<script src="../js/main.js"></script>
+<footer class="footer text-center fixed-bottom">
+  <div class="container">
+    <p>This project is made in partial fulfillment for the requirements for the sixth semester.</p>
+  </div>
+</footer>
+</body>
+
+</html>
